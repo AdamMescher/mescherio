7690019116347e878f0c0f99f0fc9be249faffd6
Adam Mescher <admescher@gmail.com>
2020-01-22 15:06:47 -0700

    Create Footer component directory and populate with index and styled tsx files

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'styled-components';
+import StyledFooter from './styled';
+
+const Footer = () => (
+  <footer>
+    <StyledFooter>
+      <h2>This is the footer</h2>
+    </StyledFooter>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Footer/styled.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.section``;
+
+export default StyledFooter;
