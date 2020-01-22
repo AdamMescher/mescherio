3c4ee3ccd96f81b27f832c6e0d72b41d1ee49186
Adam Mescher <admescher@gmail.com>
2020-01-22 15:21:34 -0700

    Update Design.stories.tsx with Color story

---
+++ src/stories/Design.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Color from '../components/Design/Color';
+
+storiesOf('Design System', module)
+  .add('Color', () => (<Color />));
