a43de83685e6bc7a26422b2eb27c804a23a699aa
Adam Mescher <admescher@gmail.com>
2020-02-06 11:15:24 -0700

    Update responsive styling for About component

--- src/components/About/styled.tsx
+++ src/components/About/styled.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const StyledAbout = styled.section`
-    height: 50vh;
     width: 100%;
     // background-color: ${({ theme }) => theme.colors.neutral.ash[200]};
     background: url(images/funky-lines.png);
@@ -15,6 +14,7 @@ const StyledAbout = styled.section`
         font-size: ${({ theme }) => theme.typography.fontSize[7]}px;
     }
     .text-and-img-container {
+        margin-top: 32px;
         height: 80%;
         width: 100%;
         display: flex;
@@ -30,6 +30,7 @@ const StyledAbout = styled.section`
         width: 40%;
         height: 100%;
         margin-right: 15%;
+        margin-left: 3%;
         display: flex;
         align-items: center;
     }
@@ -38,6 +39,36 @@ const StyledAbout = styled.section`
     }
     p {
         font-size: ${({ theme }) => theme.typography.fontSize[6]}px;
+    }
+    @media(max-width: 1024px) {
+        .text-and-img-container {
+            flex-direction: column;
+            align-items: center;
+        }
+        .img-container {
+            width: 100%;
+            margin-left: 0;
+        }
+        img {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100px;
+            width: 100%;
+        }
+        .text-container {
+            margin: 0;
+            width: 100%;
+            margin-left: 20%;
+            margin-right: 20%;
+            padding-bottom: 24px;
+        }
+        p {
+            margin-top: 24px;
+            margin-left: 20%;
+            margin-right: 20%;
+            font-size: ${({ theme }) => theme.typography.fontSize[4]}px;
+        }
     }
 `;
 
