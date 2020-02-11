d256437fd52bc41e7a67b43f468966f057e1875d
Adam Mescher <admescher@gmail.com>
2020-02-11 16:22:06 -0700

    Add favicon laptop emoji

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -7,6 +7,10 @@ const Head = () => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel="stylesheet" href="https://use.typekit.net/dfi0mui.css" />
     <meta name="description" content="Personal portfolio website for Adam Mescher, a developer in Denver, Colorado" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css.map" />
   </NextHead>
 );
