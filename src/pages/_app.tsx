4cc043147ca93d236efbf2382db01c8dbca3f21d
Adam Mescher <admescher@gmail.com>
2020-02-11 16:33:47 -0700

    Remove normalize cdn and add styled-normalize project dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 import GlobalStyle from '../components/GlobalStyle';
 import theme from '../theme';
 
@@ -10,6 +11,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <Normalize />
         <GlobalStyle />
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
